4bd7adfdacb0a985e5b1af453294d7fbcdfadcb3
mukul <mukulraw199517@gmail.com>
2018-02-26 19:51:12 +0530

    ghgjk

--- app/src/main/java/com/youthlive/youthlive/PlayerActivity.java
+++ app/src/main/java/com/youthlive/youthlive/PlayerActivity.java
@@ -70,6 +70,7 @@ import com.wowza.gocoder.sdk.api.configuration.WZMediaConfig;
 import com.wowza.gocoder.sdk.api.devices.WZAudioDevice;
 import com.wowza.gocoder.sdk.api.devices.WZCameraView;
 import com.wowza.gocoder.sdk.api.errors.WZStreamingError;
+import com.wowza.gocoder.sdk.api.h264.WZProfileLevel;
 import com.wowza.gocoder.sdk.api.player.WZPlayerConfig;
 import com.wowza.gocoder.sdk.api.player.WZPlayerView;
 import com.wowza.gocoder.sdk.api.status.WZState;
@@ -233,7 +234,6 @@ public class PlayerActivity extends AppCompatActivity implements WZStatusCallbac
 
 
         goCoder = WowzaGoCoder.init(this, "GOSK-C344-0103-177D-9E68-FCF9");
-
 
         uri = getIntent().getStringExtra("uri");
         liveId = getIntent().getStringExtra("liveId");
@@ -1559,22 +1559,54 @@ public class PlayerActivity extends AppCompatActivity implements WZStatusCallbac
 
         wzPlayerConfig = new WZPlayerConfig();
         //wzPlayerConfig.setIsPlayback(true);
+
+        wzPlayerConfig.setVideoEnabled(true);
+        wzPlayerConfig.setVideoFrameWidth(640);
+        wzPlayerConfig.setVideoFrameHeight(480);
+        wzPlayerConfig.setVideoFramerate(30);
+        wzPlayerConfig.setVideoKeyFrameInterval(30);
+        wzPlayerConfig.setVideoBitRate(1500);
+        wzPlayerConfig.setABREnabled(true);
+
+        WZProfileLevel profileLevel = new WZProfileLevel(1, 1);
+
+        if (profileLevel.validate()) {
+            wzPlayerConfig.setVideoProfileLevel(profileLevel);
+        }
+
+
+        wzPlayerConfig.setAudioEnabled(true);
+
+        wzPlayerConfig.setAudioSampleRate(44100);
+        wzPlayerConfig.setAudioChannels(1);
+        wzPlayerConfig.setAudioBitRate(64000);
+
+
         wzPlayerConfig.setHostAddress("ec2-18-219-154-44.us-east-2.compute.amazonaws.com");
         wzPlayerConfig.setApplicationName("live");
         wzPlayerConfig.setStreamName(resourceUri);
         wzPlayerConfig.setPortNumber(1935);
 
 
+        //WZStreamingError configValidationError = wzPlayerConfig.validateForPlayback();
+
+        //Log.d("Error Wowza" , configValidationError.getErrorDescription());
 
         //wzPlayerConfig.setHLSEnabled(true);
 
-        wzPlayerConfig.setAudioEnabled(true);
-        wzPlayerConfig.setVideoEnabled(true);
-        surfaceView.setScaleMode(WZMediaConfig.FILL_VIEW);
-        surfaceView.setKeepScreenOn(true);
+        if (surfaceView.isReadyToPlay())
+        {
+            wzPlayerConfig.setPreRollBufferDuration(0);
 
-        WZStatusCallback statusCallback = new StatusCallback();
-        surfaceView.play(wzPlayerConfig, statusCallback);
+            surfaceView.setLogLevel(2);
+
+            surfaceView.play(wzPlayerConfig , this);
+
+            Log.d("asdasdas" , "ready_to_play");
+
+        }
+
+
 
         /*ExtractorsFactory extractorsFactory = new DefaultExtractorsFactory();
         MediaSource videoSource = new ExtractorMediaSource(Uri.parse(ur),
